850a829d082348c5ce07c96b3a3a10c989ad8cc9
Andres Kovalev <mail@akovalev.ru>
2025-09-29 17:30:31 +0300

    Use not mine solution

--- main.js
+++ main.js
@@ -1,37 +1,60 @@
-async function run() {
-  const devices = await navigator.mediaDevices.enumerateDevices();
+'use strict';
 
-  const root = document.getElementById('container');
+var videoElement = document.querySelector('video');
+var audioSelect = document.querySelector('select#audioSource');
+var videoSelect = document.querySelector('select#videoSource');
 
-  for(const device of devices) {
-    root.appendChild(createDescriptionDiv(device));
-  }
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
 
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+getStream().then(getDevices).then(gotDevices);
 
-  for(const track of stream.getTracks()) {
-    root.appendChild(createDescriptionDiv(track));
+function getDevices() {
+  // AFAICT in Safari this only gets default devices until gUM is called :/
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos; // make available to console
+  console.log('Available input and output devices:', deviceInfos);
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    }
   }
 }
 
-function createDescriptionDiv(object) {
-  const objectDiv = document.createElement('div');
-  objectDiv.style.border = '1px solid black';
-  objectDiv.style.margin = '5px';
-
-  for(const field in object) {
-    const b = document.createElement('b');
-    b.textContent = `${field}:`;
-    const span = document.createElement('span');
-    span.textContent = object[field];
-
-    const fieldDiv = document.createElement('div');
-    fieldDiv.appendChild(b);
-    fieldDiv.appendChild(span);
-    objectDiv.appendChild(fieldDiv);
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
   }
-
-  return objectDiv;
+  const audioSource = audioSelect.value;
+  const videoSource = videoSelect.value;
+  const constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  return navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).catch(handleError);
 }
 
-run();
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  audioSelect.selectedIndex = [...audioSelect.options].
+    findIndex(option => option.text === stream.getAudioTracks()[0].label);
+  videoSelect.selectedIndex = [...videoSelect.options].
+    findIndex(option => option.text === stream.getVideoTracks()[0].label);
+  videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Error: ', error);
+}
